a5680e721984cf58686687144a37b51bc536a6c7
tacowasabii <godseoril812@gmail.com>
2024-07-02 23:27:19 +0900

    세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-[760px] h-10">
+      <button id="color-1" className="w-[55px]">
+        <img src="/color-1.png" alt="color-1 button" />
+      </button>
+      <button id="color-2" className="w-[55px]">
+        <img src="/color-2.png" alt="color-2 button" />
+      </button>
+      <button id="color-3" className="w-[55px]">
+        <img src="/color-3.png" alt="color-3 button" />
+      </button>
+      <button id="color-4" className="w-[55px]">
+        <img src="/color-4.png" alt="color-4 button" />
+      </button>
+      <button id="color-5" className="w-[55px]">
+        <img src="/color-5.png" alt="color-5 button" />
+      </button>
+      <button id="color-6" className="w-[55px]">
+        <img src="/color-6.png" alt="color-6 button" />
+      </button>
+      <button id="apply-mask" className="w-[55px]">
+        <img src="/Screen-mask-on.png" alt="Screen-mask-on button" />
+      </button>
+      <button id="remove-mask" className="w-[55px]">
+        <img src="/Screen-mask-off.png" alt="Screen-mask-off button" />
+      </button>
+      <button id="change-font-button" className="w-[55px]">
+        <img src="/Change-font.png" alt="Change-font button" />
+      </button>
+      <button id="Font-size-up" className="w-[55px]">
+        <img src="/Font-size-up.png" alt="Font-size-up button" />
+      </button>
+      <button id="Font-size-down" className="w-[55px]">
+        <img src="/Font-size-down.png" alt="Font-size-down button" />
+      </button>
+      <button id="remove-reset" className="w-[55px]">
+        <img src="/Deblurring_new.png" alt="Reset-button" />
+      </button>
+      <button id="reset" className="w-[55px]">
+        <img src="/Reset-button.png" alt="Reset-button" />
+      </button>
     </div>
   );
 }
