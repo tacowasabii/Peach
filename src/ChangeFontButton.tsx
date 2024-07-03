8807484bfea45eece2a90a33b34af209d830b9b2
tacowasabii <godseoril812@gmail.com>
2024-07-04 00:12:41 +0900

    feat: 폰트 변경 기능

---
+++ src/ChangeFontButton.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const ChangeFontButton = () => {
+  const handleClick = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tabId = tabs[0].id;
+      if (!tabs[0].url?.startsWith('http')) {
+        alert('This extension works on http and https pages only.');
+        return;
+      }
+      if (tabId !== undefined) {
+        chrome.scripting.executeScript({
+          target: { tabId },
+          func: changeFont,
+        });
+      } else {
+        console.error('Tab ID is undefined.');
+      }
+    });
+  };
+
+  const changeFont = () => {
+    const elements = document.getElementsByTagName(
+      '*',
+    ) as HTMLCollectionOf<HTMLElement>;
+
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.fontFamily = 'noto, sans-serif';
+    }
+
+    const checkExist = setInterval(function () {
+      const frame = document.getElementById(
+        'mainFrame',
+      ) as HTMLIFrameElement | null;
+      if (frame) {
+        const doc = frame.contentDocument || frame.contentWindow?.document;
+        if (doc) {
+          const frameElements = doc.getElementsByTagName(
+            '*',
+          ) as HTMLCollectionOf<HTMLElement>;
+          for (let i = 0; i < frameElements.length; i++) {
+            frameElements[i].style.fontFamily = 'noto, sans-serif';
+          }
+          clearInterval(checkExist);
+        }
+      }
+    }, 100); // check every 100ms
+  };
+
+  return (
+    <button onClick={handleClick} id="change-font-button" className="w-[55px]">
+      <img src="/Change-font.png" alt="Change font button" />
+    </button>
+  );
+};
+
+export default ChangeFontButton;
